de668f0adecd33ab69b82eac44773ab8c8df7551
shawaiz <145105002+shawaizsher@users.noreply.github.com>
2023-12-12 18:07:02 +0500

    Add project files.

---
+++ Shawaiz Sher 01 131232 082/Shawaiz Sher 01 131232 082.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int result, user_input;
+    cout << "Enter your input: ";
+    cin >> user_input;
+    if (user_input < 0)
+    {
+        result = -1;
+    }
+    else if (user_input > 0 && user_input <= 5)
+    {
+        result = 0;
+
+    }
+    else if (user_input >= 6 && user_input <= 12)
+    {
+        result = 1;
+    }
+    else if (user_input >= 13 && user_input <= 19)
+    {
+        result = 2;
+    }
+    else if (user_input >= 20 && user_input <= 50)
+    {
+        result = 3;
+    }
+    else if (user_input >= 51 && user_input <= 60)
+    {
+        result = 4;
+    }
+    else if (user_input >= 61 && user_input <= 101)
+    {
+        result = 5;
+    }
+    else if (user_input > 101)
+    {
+        result = -1;
+    }
+    cout << "Assigned value according to your input: " << result;
+}
